cec142e2525ea793f1a20f2b4c6a5125fa1a6678
An Sung Eun <bach0918@outlook.kr>
2023-05-15 04:51:01 +0900

    chart created for dashboard

--- src/components/indexBoard/IndexBoard.jsx
+++ src/components/indexBoard/IndexBoard.jsx
@@ -1,5 +1,6 @@
-import { Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import { Bar, BarChart, CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, PieChart, Pie, Cell } from 'recharts';
 import './IndexBoard.scss';
+import { useState } from 'react';
 
 const IndexBoard = () => {
     const data = [
@@ -25,19 +26,80 @@ const IndexBoard = () => {
         }
     ]
 
+    // custom tooltip
+    const customTooltip = ({ active, payload, label }) => {
+        if (active && payload && payload.length) {
+            console.log(label);
+            return (
+                <div className='custom-tooltip'>
+                    <div>{`year : ${label}`}</div>
+                    <div style={{fontSize:'13px', fontWeight:'1000'}}>{`rate :${payload[0].value}`}</div>
+                </div>
+            );
+        }        
+    };
+
+    // controll bar of the bar-chart
+    // const [barIndex, setBarIndex] = useState(null);
+    // const handleMouseEnter = (data, index) => {
+    //     setBarIndex(index);
+    // };
+    // const handleMouseLeave = () => {
+    //     setBarIndex(null);
+    // };
+
+    // controll pie chart
+    const COLORS = ['#0087fe80', '#00c4a083', '#ff814283', '#424fff83', '#ffbb2879'];
+
     return (
         <div className='index-board'>
-            <div>
-                <LineChart className='linechart' width={320} height={130} data={data}>
-                    <Line type="linear" dataKey={'value'} stroke="#8884d8" />
+            <div className='chart-wrapper'>
+                <LineChart className='linechart basic' width={320} height={130} data={data}>
+                    <Line type="linear" dataKey={'value'} stroke="orange" dot={null} />
                     <XAxis dataKey={'year'} />
-                    <YAxis />
-                    
-                    <Tooltip className='chart-tooltip' />
+                    <CartesianGrid strokeDasharray="2 2" />                    
+                    <Tooltip content={customTooltip} />
+                    <Legend verticalAlign='top' align='right' />
                 </LineChart>
             </div>
-            <div>federal rate</div>
-            <div>gdp</div>
+            <div>
+                <div className='chart-wrapper'>
+                    <BarChart className='linechart basic' width={320} height={130} data={data} >
+                        <Bar
+                            barSize={30}
+                            radius={[3, 3, 0, 0]}
+                            dataKey={'value'}
+                            fill="pink"
+                            // onMouseEnter={handleMouseEnter}
+                            // onMouseLeave={handleMouseLeave}
+                        />
+                        <XAxis dataKey={'year'} />
+                        <Tooltip cursor={{fill: '#ffc0cb63'}} content={customTooltip} />
+                        <CartesianGrid strokeDasharray="2 2" />
+                        <Legend verticalAlign='top' align='right' />
+                    </BarChart>
+                </div>
+            </div>
+            <div>
+                <div className='chart-wrapper'>
+                    <PieChart className='linechart basic' width={320} height={130}>
+                        <Pie
+                            dataKey="value"
+                            isAnimationActive={true}
+                            data={data}
+                            outerRadius={55}
+                            innerRadius={40}
+                            paddingAngle={3}
+                            label
+                        >
+                        {data.map((entry, index) => (
+                                <Cell fill={COLORS[index % COLORS.length]} />
+                            ))}
+                        </Pie>
+                        <Tooltip content={customTooltip} />
+                    </PieChart>
+                </div>
+            </div>
         </div>
     );
 };
